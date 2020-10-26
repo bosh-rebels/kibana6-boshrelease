3364f6452e554eaf760a660564b80f72475cad4b
Diego Fidalgo Moreno <Diego.FidalgoMoreno@santander.co.uk>
2020-10-26 15:26:59 +0000

    Fixing blobs

--- add-blobs.sh
+++ add-blobs.sh
@@ -2,15 +2,12 @@
 
 DIR=`pwd`
 
-mkdir -p .downloads
+mkdir -p /tmp/bosh_downloads
 
-cd .downloads
-
-KIBANA_VERSION=7.9.0
-
-if [ ! -f ${DIR}/blobs/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz ];then
-    curl -L -J -o kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/kibana/kibana-oss-${KIBANA_VERSION}-linux-x86_64.tar.gz
-    bosh add-blob --dir=${DIR} kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
-fi
-
-cd -
+pushd /tmp/bosh_downloads
+    KIBANA_VERSION=7.9.0
+    if [ ! -f ${DIR}/blobs/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz ];then
+        curl -L -J -o kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/kibana/kibana-oss-${KIBANA_VERSION}-linux-x86_64.tar.gz
+        bosh add-blob --dir=${DIR} kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
+    fi
+popd
