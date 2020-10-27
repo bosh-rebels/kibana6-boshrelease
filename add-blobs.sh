2dcfb0dd7e5ea71c1a270ad23a2fe6cc32f38d33
Mehta Deepak <deepak.mehta@servexternos.gruposantander.com>
2020-10-27 17:49:46 +0000

    should add standard kibana in blobstore

--- add-blobs.sh
+++ add-blobs.sh
@@ -7,7 +7,7 @@ mkdir -p /tmp/bosh_downloads
 pushd /tmp/bosh_downloads
     KIBANA_VERSION=7.9.0
     if [ ! -f ${DIR}/blobs/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz ];then
-        curl -L -J -o kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/kibana/kibana-oss-${KIBANA_VERSION}-linux-x86_64.tar.gz
+        curl -L -J -o kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
         bosh add-blob --dir=${DIR} kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
     fi
 popd
