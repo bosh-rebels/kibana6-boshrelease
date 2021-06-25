ac0f2100e8e464059283bfb26b49550fe7ed06b0
Diego Fidalgo Moreno <Diego.FidalgoMoreno@santander.co.uk>
2021-06-25 11:53:15 +0100

    Adapting our elk BOSH Release to work with version 6.8.*
    Signed-off-by: Pavlo Yashchurko <pavlo.yashchurko@servexternos.gruposantander.com>
    
    #178654930

--- add-blobs.sh
+++ add-blobs.sh
@@ -5,7 +5,7 @@ DIR=`pwd`
 mkdir -p /tmp/bosh_downloads
 
 pushd /tmp/bosh_downloads
-    KIBANA_VERSION=7.10.2
+    KIBANA_VERSION=6.8.16
     if [ ! -f ${DIR}/blobs/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz ];then
         curl -L -J -o kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
         bosh add-blob --dir=${DIR} kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
